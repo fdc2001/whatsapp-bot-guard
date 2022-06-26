6927032d784bc85d4bef7b4f9d9d76ef7159c0f5
Filipe Clemente <5507892-FilipeClemente@users.noreply.gitlab.com>
2022-06-26 21:33:37 +0100

    big fix 🐛

--- events/onGlobalParticipantsChanged.js
+++ events/onGlobalParticipantsChanged.js
@@ -10,8 +10,15 @@ async function handler(client) {
             if(participantChangedEvent.who!=host) {
                 const details = await client.getGroupInfo(participantChangedEvent.chat);
                 const config = await Group.findOne({chatId: participantChangedEvent.chat});
-                if(config.createdBy === participantChangedEvent.who){
-                    await client.promoteParticipant(participantChangedEvent.chat, participantChangedEvent.who);
+                if(config){
+                    if(config.createdBy === participantChangedEvent.who){
+                        await client.promoteParticipant(participantChangedEvent.chat, participantChangedEvent.who);
+                    }else{
+                        await client.sendTextWithMentions(participantChangedEvent.chat, appName + `Bem vindo @${participantChangedEvent.who} ao grupo!\nEstou aqui sempre disponivel para ajudar a manter o grupo em ordem.\nEnvio a informação presente na descrição do grupo:`);
+                        if(details.description){
+                            console.log(await client.sendText(participantChangedEvent.chat, details.description));
+                        }
+                    }
                 }else{
                     await client.sendTextWithMentions(participantChangedEvent.chat, appName + `Bem vindo @${participantChangedEvent.who} ao grupo!\nEstou aqui sempre disponivel para ajudar a manter o grupo em ordem.\nEnvio a informação presente na descrição do grupo:`);
                     if(details.description){
