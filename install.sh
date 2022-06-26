abe14d461d03955aca5297e94208a4d293133c36
Filipe Clemente <5507892-FilipeClemente@users.noreply.gitlab.com>
2022-06-26 19:19:49 +0100

    add script for install

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt install libnss3-dev libatk-bridge2.0-0 libxkbcommon-x11-0 libgtk-3-0 libgbm-dev build-essential nodejs
